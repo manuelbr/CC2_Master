86fc1cbd01c5a061e24cd9d23072fa70a88eb3f2
manuelbr <manuelbr@correo.ugr.es>
2017-03-23 19:55:54 +0100

    Actualización de la práctica 1. close #3

--- practica_1/src/php/consultar.php
+++ practica_1/src/php/consultar.php
@@ -1,35 +1,25 @@
 <?php
 
 function consulta($query){
-        $enlace = new mysqli("localhost","root","1234","textos");
-        //Si la conexión a la base de datos ha fallado, que muestre un error:
-        if($enlace->connect_errno) {
-            echo "Fallo al contenctar a MySQL: (" . $enlace->connect_errno . ")";
-        }else{
-             //Seleccionamos la Base de Datos en la que vamos a trabajar
-             $enlace->select_db("textos");
-             //Realizamos la consulta deseada
-             $resultado_consulta = $enlace->query($query);
+  $resultado = "mal";
+  $enlace = new mysqli("localhost","root","1234","textos");
+  //Si la conexión a la base de datos ha fallado, que muestre un error:
+  if($enlace->connect_errno) {
+      echo "Fallo al conectar a MySQL: (" . $enlace->connect_errno . ")";
+  }else{
+    //Seleccionamos la Base de Datos en la que vamos a trabajar
+    $enlace->select_db("textos");
+    //Realizamos la consulta deseada
+    $resultado_consulta = $enlace->query($query);
 
-             if(!$resultado_consulta){
-               echo 'Se ha producido un error al ejecutar la consulta a la base de datos: '.$enlace->error;
-             }
-
-             if($query != "SELECT * FROM textos"){
-               $resultado_consulta = $enlace->query("SELECT * FROM textos");
-
-               if(!$resultado_consulta){
-                 echo 'Se ha producido un error al listar los elementos de la base de datos: '.$enlace->error;
-               }
-             }
-
-             //Mostramos el resultado de nuestra consulta sobre los elementos de la base de datos
-             echo '<ul>';
-             while($row = $resultado_consulta->fetch_assoc()){
-                    echo '<li>'.$row["texto"].'<a href></li>';
-             }
-             echo '</ul>';
-        }
+    if(!$resultado_consulta){
+      echo 'Se ha producido un error al ejecutar la consulta a la base de datos: '.$enlace->error;
+    }else{
+      $resultado = "bien";
+    }
     mysqli_close($enlace);
+  }
+
+  return $resultado;
 }
 ?>
